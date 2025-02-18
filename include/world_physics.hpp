e82d48ba585d2948c15d515e07dc96301c432dca
Shivang Patel <shivaang14@gmail.com>
2025-02-17 19:12:33 -0500

    update circle collision code as per new update to body class

--- include/world_physics.hpp
+++ include/world_physics.hpp
@@ -10,27 +10,30 @@
 class WorldPhysics {
 
   public:
-    void handleCollision(std::vector<Body> &body_list) {
+    void handleCollision(std::vector<std::shared_ptr<Body>> &body_list) {
         // Right now handling in brute force way
 
         for (int i = 0; i < body_list.size(); ++i) {
 
+            auto circle_body_ptr_i = std::static_pointer_cast<CircleBody>(body_list[i]);
+
             for (int j = 0; j < body_list.size(); ++j) {
+
                 if (i != j) {
+                    auto circle_body_ptr_j = std::static_pointer_cast<CircleBody>(body_list[j]);
                     // Check for circle collision
-                    const auto distance = calculateLengthOfVector(body_list[i].getPosition() -
-                                                                  body_list[j].getPosition());
-                    if (distance < body_list[i].radius_ + body_list[j].radius_) {
+                    const auto distance = calculateLengthOfVector(circle_body_ptr_i->getPosition() - circle_body_ptr_j->getPosition());
+                    if (distance < circle_body_ptr_i->radius_ + circle_body_ptr_j->radius_) {
                         auto amount_of_collision =
-                            (body_list[i].radius_ + body_list[j].radius_) - distance + 1.f;
+                            (circle_body_ptr_i->radius_ + circle_body_ptr_j->radius_) - distance + 1.f;
 
                         const auto direction_of_collision =
-                            calculateUnitVector(body_list[i].getPosition() - body_list[j].getPosition());
+                            calculateUnitVector(circle_body_ptr_i->getPosition() - circle_body_ptr_j->getPosition());
 
                         // Move the bodies according to direction and magnitude
-                        body_list[i].movePosition(direction_of_collision *
+                        circle_body_ptr_i->updatePosition(direction_of_collision *
                                                   (amount_of_collision / 2.0f) * 1.0f);
-                        body_list[j].movePosition(direction_of_collision *
+                        circle_body_ptr_j->updatePosition(direction_of_collision *
                                                   (amount_of_collision / 2.0f) * -1.0f);
                     }
                 }
